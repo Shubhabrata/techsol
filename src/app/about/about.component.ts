938ee33aa4f776199f0c030dc5a3639a85cf7418
Das, Shubhabrata (US - Bengaluru) <shudas@deloitte.com>
2017-09-21 20:08:16 +0530

    updates.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'my-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
 
   constructor() {
     // Do stuff
@@ -14,5 +14,14 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     console.log('Hello About');
   }
+  ngAfterViewInit() {
+    (<any>$('.scrollspy')).scrollSpy();
+    /*(<any>$('.carousel-prod')).carousel({ fullWidth: true }, setTimeout(autoplay, 8000));    
+    function autoplay() {
+      (<any>$('.carousel-prod')).carousel('next');
+      setTimeout(autoplay, 8000);
+    }*/
+  }
+  
 
 }
